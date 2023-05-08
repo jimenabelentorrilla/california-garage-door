cdc314228895cb89cea2265489b285826c0aefe8
Jimena Belen <jiment4@hotmail.com>
2023-05-07 22:26:31 -0300

    header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
-
+import PointRed from "../assets/Point.png";
+import Google from "../assets/Frame 1047.png";
+import Yelp from "../assets/Frame 1048.png";
+import Tel from "../assets/Frame 1422.png"
 function Header() {
   return (
     <div className='header-phone'>
@@ -9,23 +11,54 @@ function Header() {
             <p>Serving homeowners all over Texas</p>
         </div>
         <div>
-            <ul>
-                <li><span>24/7</span>Emergency Service Available</li>
-                <li>Licensed and Insured</li>
-                <li>Your Local Garage Door Repair Company</li>
-                <li>Same Day Service Available</li>
-            </ul>
+            <div className='frame1438'>
+                <div className='li-flex'>
+                    <div>
+                        <img src={PointRed} />
+                    </div>
+                    <div>
+                        <p>24/7 Emergency Service Available</p>
+                    </div>
+                </div>
+                <div className='li-flex'>
+                    <div>
+                        <img src={PointRed} />
+                    </div>
+                    <div>
+                        <p>Licensed and Insured</p>
+                    </div>
+                </div>
+                <div className='li-flex'>
+                    <div>
+                        <img src={PointRed} />
+                    </div>
+                    <div>
+                        <p>Your Local Garage Door <br />Repair Company</p>
+                    </div>
+                </div>
+                <div className='li-flex'>
+                    <div>
+                        <img src={PointRed} />
+                    </div>
+                    <div>
+                        <p>Same Day Service Available</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
+        <div className='frame1049'>
             <div>
-                <img></img>
+                <img src={Yelp} />
             </div>
             <div>
-                <img></img>
+                <img src={Google} />
             </div>
         </div>
-        <div>
-            <button></button>
+        <div className='frame136'>
+            <div className='frame1063'>
+                <img src={Tel}/>
+                <p>999-999-9999</p>
+            </div>
         </div>
     </div>
   )
